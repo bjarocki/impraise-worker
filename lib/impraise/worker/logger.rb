d607fdb7c4b433c545ea4e346cd1f58b26496ce5
Bartek Jarocki <bartek@smatly.com>
2017-09-19 17:11:05 +0100

    [CORE] worker gets logger class
    - to have some fun with web console I'm sending _logs_ objects to redis
    pub/sub and expose them with logger-console :two_hearts:

---
+++ lib/impraise/worker/logger.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'impraise/worker/requirements'
+
+module Impraise
+  module Worker
+    class Logger
+      def initialize
+        @config = Impraise::Worker::Config.new
+        @dns = Impraise::Worker::DNS
+
+        # discover redis service and setup client
+        host, port = @dns.disco('redis')
+        @redis = ::Redis.new(host: host, port: port, db: 0)
+      end
+
+      def channel
+        'impraise-debug'
+      end
+
+      def publish(message)
+        puts "RAW => #{message}"
+        puts sanitize(hostname: Socket.gethostname, body: message)
+        @redis.publish(channel, sanitize(hostname: Socket.gethostname, body: message))
+      end
+
+      private
+
+      def sanitize(message)
+        json = JSON.parse(JSON.generate(message))
+        json.each { |key, value| json[key] = ERB::Util.html_escape(value) }
+        JSON.generate(json)
+      end
+    end
+  end
+end
